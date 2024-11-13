d925b3408696f0e9175d2b0346c9dbbdc9f3d698
whatsmynameidontknow <akunhantusaya@gmail.com>
2024-11-13 15:15:23 +0700

    feat: other

--- src/main/java/ogya/lokakarya/be/entity/AssessmentSummary.java
+++ src/main/java/ogya/lokakarya/be/entity/AssessmentSummary.java
@@ -5,6 +5,11 @@ import java.time.LocalDate;
 import java.util.UUID;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -12,11 +17,23 @@ import lombok.Data;
 @Entity
 @Table(name = "TBL_ASSESSMENT_SUMMARY")
 public class AssessmentSummary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    private UUID userId;
-    private Integer number;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "YEAR", nullable = false)
+    private Integer year;
+
+    @Column(name = "SCORE", nullable = false)
     private Integer score;
+
+    @Column(name = "status", nullable = false)
     private Integer status;
+
     @Column(name = "CREATED_AT", nullable = false)
     private Date createdAt = Date.valueOf(LocalDate.now());
 
--- src/main/java/ogya/lokakarya/be/entity/User.java
+++ src/main/java/ogya/lokakarya/be/entity/User.java
@@ -2,12 +2,15 @@ package ogya.lokakarya.be.entity;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -52,4 +55,7 @@ public class User {
 
     @Column(name = "UPDATED_BY")
     private UUID updatedBy;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<AssessmentSummary> assessments;
 }
