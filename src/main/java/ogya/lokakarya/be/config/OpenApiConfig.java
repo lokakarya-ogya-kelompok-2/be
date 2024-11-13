ae13f0400ef9cfce5ad98c6b7aa037129b4b183f
whatsmynameidontknow <akunhantusaya@gmail.com>
2024-11-13 16:23:12 +0700

    feat: add swagger

---
+++ src/main/java/ogya/lokakarya/be/config/OpenApiConfig.java
@@ -0,0 +1,37 @@
+package ogya.lokakarya.be.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(final @NonNull ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/swagger-ui.html");
+        registry.addRedirectViewController("/swagger-ui", "/swagger-ui.html");
+    }
+
+    @Bean
+    OpenAPI openAPI() {
+        return new OpenAPI().components(new Components())
+                .info(new Info().title("OGYA REST API").description("REST API for something")
+                        .version("1.0.0").termsOfService("ToS")
+                        .license(new License().name("Apache License Version 2.0")
+                                .url("https:/apache.org/licenses/LICENSE-2.0"))
+                        .contact(new Contact().name("aku").email("aku@email.com")));
+    }
+
+    @Bean
+    GroupedOpenApi allApis() {
+        return GroupedOpenApi.builder().group("*").pathsToMatch("/**").build();
+    }
+}
