4a10a67685b18576bc969d97625e58e62145cd03
whatsmynameidontknow <44807964+whatsmynameidontknow@users.noreply.github.com>
2024-11-18 14:39:01 +0700

    add division_id on create (#16)
    * feat(user): add include/exclude roles param
    
    * feat(user): add division_id on create user endpoint

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
+-- division
+INSERT IGNORE INTO
+    tbl_division(id, division_name)
+VALUES
+    (UUID(), 'Human Resource (HR)');
+
 -- roles
 INSERT IGNORE INTO
     tbl_app_role(id, rolename)
@@ -9,12 +15,15 @@ VALUES
 
 -- HR account (HR123)
 INSERT IGNORE INTO
-    tbl_app_user(id, username, full_name, email_address, position, employee_status, password, join_date)
+    tbl_app_user(id, username, full_name, email_address, division_id, position, employee_status, password, join_date)
 VALUES
-    (UUID(), 'HR01', 'Ini Akun HR', 'hr@gmail.com', 'Human Resource', 1, '$2a$12$GQRyq3chJg7ODtK0u/U/MOLjlkM8Zrr7EtxsTN3L9yaO1Y8xoYGwu', '2023-01-01');
+    (UUID(), 'HR01', 'Ini Akun HR', 'hr@gmail.com',
+     (SELECT id FROM tbl_division WHERE division_name = 'Human Resource (HR)' LIMIT 1), 'Human Resource', 1,
+     '$2a$12$GQRyq3chJg7ODtK0u/U/MOLjlkM8Zrr7EtxsTN3L9yaO1Y8xoYGwu', '2023-01-01');
 
 -- add HR role to HR account
 INSERT IGNORE INTO
     tbl_app_user_role(id, user_id, role_id)
 VALUES
-    (UUID(), (SELECT id FROM tbl_app_user WHERE username = 'HR01'), (SELECT id FROM tbl_app_role WHERE rolename = 'HR'));
+    (UUID(), (SELECT id FROM tbl_app_user WHERE username = 'HR01' LIMIT 1),
+     (SELECT id FROM tbl_app_role WHERE rolename = 'HR' LIMIT 1));
