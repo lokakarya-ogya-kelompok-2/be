cc532fe76290e30018f84a19742af0dd657d18ce
kharis <kharisrahmat686@gmail.com>
2024-11-18 13:36:32 +0700

    feat(assessment summary): indclude user id

--- src/main/java/ogya/lokakarya/be/controller/TechnicalSkillController.java
+++ src/main/java/ogya/lokakarya/be/controller/TechnicalSkillController.java
@@ -1,14 +1,20 @@
 package ogya.lokakarya.be.controller;
 
 import jakarta.validation.Valid;
-import ogya.lokakarya.be.dto.technicalskill.TechnicalSkillReq;
 import ogya.lokakarya.be.dto.technicalskill.TechnicalSkillDto;
-import ogya.lokakarya.be.entity.TechnicalSkill;
+import ogya.lokakarya.be.dto.technicalskill.TechnicalSkillReq;
 import ogya.lokakarya.be.service.TechnicalSkillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
@@ -20,7 +26,7 @@ public class TechnicalSkillController {
     private TechnicalSkillService technicalSkillService;
 
     @PostMapping
-    public ResponseEntity<TechnicalSkill> create(@RequestBody @Valid TechnicalSkillReq data) {
+    public ResponseEntity<TechnicalSkillDto> create(@RequestBody @Valid TechnicalSkillReq data) {
         var createdDivision= technicalSkillService.create(data);
         return new ResponseEntity<>(createdDivision, HttpStatus.CREATED);
     }
