af6c5953afab474c6b6435f561c29c49bba8e1ca
kharis <kharisrahmat686@gmail.com>
2024-11-18 13:36:32 +0700

    feat(assessment summary): indclude user id

--- src/main/java/ogya/lokakarya/be/dto/assessmentsummary/AssessmentSummaryDto.java
+++ src/main/java/ogya/lokakarya/be/dto/assessmentsummary/AssessmentSummaryDto.java
@@ -1,5 +1,7 @@
 package ogya.lokakarya.be.dto.assessmentsummary;
 
+import java.util.Date;
+import java.util.UUID;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,53 +11,50 @@ import lombok.ToString;
 import ogya.lokakarya.be.dto.user.UserDto;
 import ogya.lokakarya.be.entity.AssessmentSummary;
 
-import java.util.Date;
-import java.util.UUID;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
 @ToString
 public class AssessmentSummaryDto {
-    @JsonProperty("id")
-    private UUID id;
+        @JsonProperty("id")
+        private UUID id;
 
-    @JsonProperty("user_id")
-    private UserDto user;
+        @JsonProperty("user_id")
+        private UserDto user;
 
-    @JsonProperty("year")
-    private Integer year;
+        @JsonProperty("year")
+        private Integer year;
 
-    @JsonProperty("score")
-    private Integer score;
+        @JsonProperty("score")
+        private Integer score;
 
-    @JsonProperty("status")
-    private Integer status;
+        @JsonProperty("status")
+        private Integer status;
 
-    @JsonProperty("created_at")
-    private Date createdAt;
+        @JsonProperty("created_at")
+        private Date createdAt;
 
-    @JsonProperty("CREATED_BY")
-    private UUID createdBy;
+        @JsonProperty("CREATED_BY")
+        private UUID createdBy;
 
-    @JsonProperty("UPDATED_AT")
-    private Date updatedAt;
+        @JsonProperty("UPDATED_AT")
+        private Date updatedAt;
 
-    @JsonProperty("UPDATED_BY")
-    private UUID updatedBy;
+        @JsonProperty("UPDATED_BY")
+        private UUID updatedBy;
 
-    public AssessmentSummaryDto(AssessmentSummary assessmentSummary) {
-        setId(assessmentSummary.getId());
-        if (assessmentSummary.getUser() != null) {
-        setUser(new UserDto(assessmentSummary.getUser(), false, false, false));
+        public AssessmentSummaryDto(AssessmentSummary assessmentSummary) {
+                setId(assessmentSummary.getId());
+                if (assessmentSummary.getUser() != null) {
+                        setUser(new UserDto(assessmentSummary.getUser(), false, false, false));
+                }
+                setYear(assessmentSummary.getYear());
+                setScore(assessmentSummary.getScore());
+                setStatus(assessmentSummary.getStatus());
+                setCreatedAt(assessmentSummary.getCreatedAt());
+                setCreatedBy(assessmentSummary.getCreatedBy());
+                setUpdatedAt(assessmentSummary.getUpdatedAt());
+                setUpdatedBy(assessmentSummary.getUpdatedBy());
         }
-        setYear(assessmentSummary.getYear());
-        setScore(assessmentSummary.getScore());
-        setStatus(assessmentSummary.getStatus());
-        setCreatedAt(assessmentSummary.getCreatedAt());
-        setCreatedBy(assessmentSummary.getCreatedBy());
-        setUpdatedAt(assessmentSummary.getUpdatedAt());
-        setUpdatedBy(assessmentSummary.getUpdatedBy());
-    }
 }
