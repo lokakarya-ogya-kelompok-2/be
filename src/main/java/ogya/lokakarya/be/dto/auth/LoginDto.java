68890540d1a8ce00a1cab14c07eccf8c116414d1
whatsmynameidontknow <44807964+whatsmynameidontknow@users.noreply.github.com>
2024-11-20 11:22:36 +0700

    login now only have 2 fields (username/email and password) (#27)

--- src/main/java/ogya/lokakarya/be/dto/auth/LoginDto.java
+++ src/main/java/ogya/lokakarya/be/dto/auth/LoginDto.java
@@ -1,61 +1,17 @@
 package ogya.lokakarya.be.dto.auth;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.Constraint;
-import jakarta.validation.ConstraintValidator;
-import jakarta.validation.ConstraintValidatorContext;
-import jakarta.validation.Payload;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
-@EmailOrUsername
 public class LoginDto {
-    @Email
-    @JsonProperty("email")
-    private String email;
-
-    @Pattern(regexp = "^[a-zA-Z0-9._]+$",
-            message = "Username can only contain alphanumeric characters, underscores, and dots")
-    @JsonProperty("username")
-    private String username;
+    @JsonProperty("email_or_username")
+    @Pattern(regexp = "^[^\\s]+$", message = "Email or Username cannot contain spaces")
+    private String emailOrUsername;
 
     @NotBlank
     @JsonProperty("password")
     private String password;
-}
-
-
-class EmailOrUsernameValidator implements ConstraintValidator<EmailOrUsername, LoginDto> {
-
-    @Override
-    public void initialize(EmailOrUsername constraintAnnotation) {}
-
-    @Override
-    public boolean isValid(LoginDto loginDto, ConstraintValidatorContext context) {
-        return ((loginDto.getEmail() != null && !loginDto.getEmail().isEmpty())
-                && (loginDto.getUsername() == null || loginDto.getUsername().isEmpty())
-                || (loginDto.getUsername() != null && !loginDto.getUsername().isEmpty())
-                        && (loginDto.getEmail() == null || loginDto.getEmail().isEmpty()));
-    }
-}
-
-
-@Constraint(validatedBy = EmailOrUsernameValidator.class)
-@Target({ElementType.TYPE})
-@Retention(RetentionPolicy.RUNTIME)
-@interface EmailOrUsername {
-    String message()
-
-    default "Either one of email or username must be provided";
-
-    Class<?>[] groups() default {};
-
-    Class<? extends Payload>[] payload() default {};
 }
