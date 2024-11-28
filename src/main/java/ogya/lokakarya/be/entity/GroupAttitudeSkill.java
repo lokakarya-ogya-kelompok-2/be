425f142d2524ee67a8cd879cb20ff0154f64f934
whatsmynameidontknow <akunhantusaya@gmail.com>
2024-11-28 14:54:06 +0700

    feat: something about attitude

--- src/main/java/ogya/lokakarya/be/entity/GroupAttitudeSkill.java
+++ src/main/java/ogya/lokakarya/be/entity/GroupAttitudeSkill.java
@@ -1,5 +1,8 @@
 package ogya.lokakarya.be.entity;
 
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,23 +13,19 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
-
 @Entity
 @Data
-@Table(name="TBL_GROUP_ATTITUDE_SKILL")
+@Table(name = "TBL_GROUP_ATTITUDE_SKILL")
 public class GroupAttitudeSkill {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name="ID")
+    @Column(name = "ID")
     private UUID id;
 
-    @Column(name ="GROUP_NAME" , length = 100)
+    @Column(name = "GROUP_NAME", length = 100)
     private String groupName;
 
-    @Column(name ="PERCENTAGE", nullable = false, length = 3)
+    @Column(name = "PERCENTAGE", nullable = false, length = 3)
     private Integer percentage;
 
     @Column(name = "ENABLED")
@@ -44,6 +43,6 @@ public class GroupAttitudeSkill {
     @Column(name = "UPDATED_BY")
     private UUID updatedBy;
 
-    @OneToMany(mappedBy = "groupAttitudeSkill", fetch = FetchType.EAGER)
-    private List<AttitudeSkill> attitudeSkill;
+    @OneToMany(mappedBy = "groupAttitudeSkill", fetch = FetchType.LAZY)
+    private List<AttitudeSkill> attitudeSkills;
 }
