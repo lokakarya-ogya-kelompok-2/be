b6f7d87162b664505ddc3f6420632fb165e86364
kharis <kharisrahmat686@gmail.com>
2024-12-15 13:01:55 +0700

    fix: fix for pull

--- src/main/java/ogya/lokakarya/be/config/security/SecurityUtil.java
+++ src/main/java/ogya/lokakarya/be/config/security/SecurityUtil.java
@@ -1,16 +1,14 @@
 package ogya.lokakarya.be.config.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import ogya.lokakarya.be.entity.User;
+import ogya.lokakarya.be.exception.ResponseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import ogya.lokakarya.be.entity.User;
-import ogya.lokakarya.be.exception.ResponseException;
 
 @Component
 public class SecurityUtil {
-    @Autowired
 
     public User getCurrentUser() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
