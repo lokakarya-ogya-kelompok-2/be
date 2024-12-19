77faaac5959e838e615e922ed8ef76a60ecc80b2
whatsmynameidontknow <akunhantusaya@gmail.com>
2024-12-19 12:00:25 +0700

    feat: validasi role menu

--- src/main/java/ogya/lokakarya/be/repository/MenuRepository.java
+++ src/main/java/ogya/lokakarya/be/repository/MenuRepository.java
@@ -1,10 +1,12 @@
 package ogya.lokakarya.be.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ogya.lokakarya.be.dto.menu.MenuFilter;
 import ogya.lokakarya.be.entity.Menu;
 
 public interface MenuRepository
-        extends JpaRepository<Menu, UUID>, FilterRepository<Menu, MenuFilter> {
+                extends JpaRepository<Menu, UUID>, FilterRepository<Menu, MenuFilter> {
+        Optional<Menu> findByMenuName(String name);
 }
