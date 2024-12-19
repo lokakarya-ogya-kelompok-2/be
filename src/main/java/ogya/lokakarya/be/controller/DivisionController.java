f1f88f22aa0826b524b5da48ed7063ad25937bb8
whatsmynameidontknow <akunhantusaya@gmail.com>
2024-12-19 20:38:49 +0700

    feat: division pagination

--- src/main/java/ogya/lokakarya/be/controller/DivisionController.java
+++ src/main/java/ogya/lokakarya/be/controller/DivisionController.java
@@ -3,6 +3,7 @@ package ogya.lokakarya.be.controller;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -28,64 +29,76 @@ import ogya.lokakarya.be.service.DivisionService;
 @RestController
 @RequestMapping("/divisions")
 public class DivisionController {
-    @Autowired
-    DivisionService divisionService;
+        @Autowired
+        DivisionService divisionService;
 
-    @PostMapping
-    public ResponseEntity<ResponseDto<DivisionDto>> create(@RequestBody @Valid DivisionReq data) {
-        log.info("Starting DivisionController.create");
-        var createdDivision = divisionService.create(data);
-        log.info("Ending DivisionController.create");
-        return ResponseDto.<DivisionDto>builder().success(true).content(createdDivision)
-                .message("Create division successful!").build().toResponse(HttpStatus.CREATED);
-    }
+        @PostMapping
+        public ResponseEntity<ResponseDto<DivisionDto>> create(
+                        @RequestBody @Valid DivisionReq data) {
+                log.info("Starting DivisionController.create");
+                var createdDivision = divisionService.create(data);
+                log.info("Ending DivisionController.create");
+                return ResponseDto.<DivisionDto>builder().success(true).content(createdDivision)
+                                .message("Create division successful!").build()
+                                .toResponse(HttpStatus.CREATED);
+        }
 
-    @GetMapping
-    public ResponseEntity<ResponseDto<List<DivisionDto>>> getAllDivisions(
-            @RequestParam(name = "name_contains", required = false) String nameContains,
-            @RequestParam(name = "with_created_by", required = false,
-                    defaultValue = "false") Boolean withCreatedBy,
-            @RequestParam(name = "with_updated_by", required = false,
-                    defaultValue = "false") Boolean withUpdatedBy) {
-        log.info("Starting DivisionController.list");
-        DivisionFilter filter = new DivisionFilter();
-        filter.setNameContains(nameContains);
-        filter.setWithCreatedBy(withCreatedBy);
-        filter.setWithUpdatedBy(withUpdatedBy);
-        List<DivisionDto> divisions = divisionService.getAllDivisions(filter);
-        log.info("Ending DivisionController.list");
-        return ResponseDto.<List<DivisionDto>>builder().success(true).content(divisions)
-                .message("List all division successful!").build().toResponse(HttpStatus.OK);
-    }
+        @GetMapping
+        public ResponseEntity<ResponseDto<List<DivisionDto>>> getAllDivisions(
+                        @RequestParam(name = "name_contains", required = false) String nameContains,
+                        @RequestParam(name = "page_number", required = false) Integer pageNumber,
+                        @RequestParam(name = "page_size", required = false,
+                                        defaultValue = "5") Integer pageSize,
+                        @RequestParam(name = "with_created_by", required = false,
+                                        defaultValue = "false") Boolean withCreatedBy,
+                        @RequestParam(name = "with_updated_by", required = false,
+                                        defaultValue = "false") Boolean withUpdatedBy) {
+                log.info("Starting DivisionController.list");
+                DivisionFilter filter = new DivisionFilter();
+                filter.setPageNumber(pageNumber);
+                filter.setPageSize(pageSize);
+                filter.setNameContains(nameContains);
+                filter.setWithCreatedBy(withCreatedBy);
+                filter.setWithUpdatedBy(withUpdatedBy);
+                Page<DivisionDto> divisions = divisionService.getAllDivisions(filter);
+                log.info("Ending DivisionController.list");
+                return ResponseDto.<List<DivisionDto>>builder().success(true)
+                                .content(divisions.toList()).totalPages(divisions.getTotalPages())
+                                .pageSize(divisions.getSize()).pageNumber(divisions.getNumber() + 1)
+                                .totalRecords(divisions.getTotalElements())
+                                .message("List all division successful!").build()
+                                .toResponse(HttpStatus.OK);
+        }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ResponseDto<DivisionDto>> getDivisionById(@PathVariable UUID id) {
-        log.info("Starting DivisionController.get for id = {}", id);
-        DivisionDto division = divisionService.getDivisionById(id);
-        log.info("Ending DivisionController.get for id = {}", id);
-        return ResponseDto.<DivisionDto>builder().success(true).content(division)
-                .message(String.format("Get division with id %s successful!", id)).build()
-                .toResponse(HttpStatus.OK);
-    }
+        @GetMapping("/{id}")
+        public ResponseEntity<ResponseDto<DivisionDto>> getDivisionById(@PathVariable UUID id) {
+                log.info("Starting DivisionController.get for id = {}", id);
+                DivisionDto division = divisionService.getDivisionById(id);
+                log.info("Ending DivisionController.get for id = {}", id);
+                return ResponseDto.<DivisionDto>builder().success(true).content(division)
+                                .message(String.format("Get division with id %s successful!", id))
+                                .build().toResponse(HttpStatus.OK);
+        }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<ResponseDto<DivisionDto>> updateDivisionById(@PathVariable UUID id,
-            @RequestBody @Valid DivisionReq divisionReq) {
-        log.info("Starting DivisionController.update for id = {}", id);
-        DivisionDto updatedDivision = divisionService.updateDivisionById(id, divisionReq);
-        log.info("Ending DivisionController.update for id = {}", id);
-        return ResponseDto.<DivisionDto>builder().success(true).content(updatedDivision)
-                .message(String.format("Update division with id %s successful!", id)).build()
-                .toResponse(HttpStatus.OK);
-    }
+        @PutMapping("/{id}")
+        public ResponseEntity<ResponseDto<DivisionDto>> updateDivisionById(@PathVariable UUID id,
+                        @RequestBody @Valid DivisionReq divisionReq) {
+                log.info("Starting DivisionController.update for id = {}", id);
+                DivisionDto updatedDivision = divisionService.updateDivisionById(id, divisionReq);
+                log.info("Ending DivisionController.update for id = {}", id);
+                return ResponseDto.<DivisionDto>builder().success(true).content(updatedDivision)
+                                .message(String.format("Update division with id %s successful!",
+                                                id))
+                                .build().toResponse(HttpStatus.OK);
+        }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<ResponseDto<Void>> deleteDivisionById(@PathVariable UUID id) {
-        log.info("Starting DivisionController.delete for id = {}", id);
-        divisionService.deleteDivisionById(id);
-        log.info("Ending DivisionController.delete for id = {}", id);
-        return ResponseDto.<Void>builder().success(true)
-                .message(String.format("Delete division with id %s successful!", id)).build()
-                .toResponse(HttpStatus.OK);
-    }
+        @DeleteMapping("/{id}")
+        public ResponseEntity<ResponseDto<Void>> deleteDivisionById(@PathVariable UUID id) {
+                log.info("Starting DivisionController.delete for id = {}", id);
+                divisionService.deleteDivisionById(id);
+                log.info("Ending DivisionController.delete for id = {}", id);
+                return ResponseDto.<Void>builder().success(true).message(
+                                String.format("Delete division with id %s successful!", id)).build()
+                                .toResponse(HttpStatus.OK);
+        }
 }
