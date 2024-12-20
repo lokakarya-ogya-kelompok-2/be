bc78796401be2d5859ed7ddf1d8eca472cf1e7a5
whatsmynameidontknow <akunhantusaya@gmail.com>
2024-12-20 19:42:27 +0700

    refactor: division

--- src/main/java/ogya/lokakarya/be/repository/specification/DivisionSpecification.java
+++ src/main/java/ogya/lokakarya/be/repository/specification/DivisionSpecification.java
@@ -1,35 +1,17 @@
 package ogya.lokakarya.be.repository.specification;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.lang.NonNull;
-import org.springframework.lang.Nullable;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
-import ogya.lokakarya.be.dto.division.DivisionFilter;
+import org.springframework.stereotype.Component;
 import ogya.lokakarya.be.entity.Division;
 
 @SuppressWarnings("java:S1118")
+@Component
 public class DivisionSpecification {
-    public static Specification<Division> filter(DivisionFilter filter) {
-        return new Specification<Division>() {
+    @Autowired
+    private SpecificationFactory<Division> spec;
 
-            @Override
-            @Nullable
-            public Predicate toPredicate(@NonNull Root<Division> root,
-                    @Nullable CriteriaQuery<?> query, @NonNull CriteriaBuilder cb) {
-                List<Predicate> predicates = new ArrayList<>();
-
-                if (filter.getNameContains() != null) {
-                    predicates.add(cb.like(cb.lower(root.get("divisionName")),
-                            "%" + filter.getNameContains().toLowerCase() + "%"));
-                }
-
-                return cb.and(predicates.toArray(new Predicate[predicates.size()]));
-            }
-        };
+    public Specification<Division> nameContains(String substr) {
+        return spec.fieldContains("divisionName", substr);
     }
 }
