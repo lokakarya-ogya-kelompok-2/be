91c56f1b573919983c959fd2863f3ab627a6271f
whatsmynameidontknow <akunhantusaya@gmail.com>
2024-12-23 14:24:38 +0700

    refactor: warning warning

--- src/main/java/ogya/lokakarya/be/dto/technicalskill/TechnicalSkillFilter.java
+++ src/main/java/ogya/lokakarya/be/dto/technicalskill/TechnicalSkillFilter.java
@@ -1,9 +1,11 @@
 package ogya.lokakarya.be.dto.technicalskill;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import ogya.lokakarya.be.dto.common.Filter;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class TechnicalSkillFilter extends Filter {
     private String nameContains;
     private Boolean enabledOnly = false;
