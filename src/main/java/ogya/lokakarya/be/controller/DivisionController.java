28c82870f0ddd25eace0a97334dd5290df77b6bd
kharis <kharisrahmat686@gmail.com>
2024-12-23 10:58:22 +0700

    feat(division): sort

--- src/main/java/ogya/lokakarya/be/controller/DivisionController.java
+++ src/main/java/ogya/lokakarya/be/controller/DivisionController.java
@@ -1,9 +1,16 @@
 package ogya.lokakarya.be.controller;
 
-import java.util.List;
-import java.util.UUID;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import ogya.lokakarya.be.dto.ResponseDto;
+import ogya.lokakarya.be.dto.division.DivisionDto;
+import ogya.lokakarya.be.dto.division.DivisionFilter;
+import ogya.lokakarya.be.dto.division.DivisionReq;
+import ogya.lokakarya.be.service.DivisionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,14 +22,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import jakarta.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
-import ogya.lokakarya.be.dto.ResponseDto;
-import ogya.lokakarya.be.dto.division.DivisionDto;
-import ogya.lokakarya.be.dto.division.DivisionFilter;
-import ogya.lokakarya.be.dto.division.DivisionReq;
-import ogya.lokakarya.be.service.DivisionService;
+
+import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 @SecurityRequirement(name = "bearerAuth")
@@ -45,14 +47,16 @@ public class DivisionController {
 
         @GetMapping
         public ResponseEntity<ResponseDto<List<DivisionDto>>> getAllDivisions(
-                        @RequestParam(name = "name_contains", required = false) String nameContains,
-                        @RequestParam(name = "page_number", required = false) Integer pageNumber,
-                        @RequestParam(name = "page_size", required = false,
+                @RequestParam(name = "name_contains", required = false) String nameContains,
+                @RequestParam(name = "page_number", required = false) Integer pageNumber,
+                @RequestParam(name = "page_size", required = false,
                                         defaultValue = "5") Integer pageSize,
-                        @RequestParam(name = "with_created_by", required = false,
+                @RequestParam(name = "with_created_by", required = false,
                                         defaultValue = "false") Boolean withCreatedBy,
-                        @RequestParam(name = "with_updated_by", required = false,
-                                        defaultValue = "false") Boolean withUpdatedBy) {
+                @RequestParam(name = "with_updated_by", required = false,
+                                        defaultValue = "false") Boolean withUpdatedBy,
+                @RequestParam(name= "sort_field", required=false, defaultValue = "createdAt") String sortField,
+                @RequestParam(name = "sort_direction", required = false, defaultValue = "DESC") Sort.Direction sortDirection) {
                 log.info("Starting DivisionController.list");
                 DivisionFilter filter = new DivisionFilter();
                 filter.setPageNumber(pageNumber);
@@ -60,6 +64,8 @@ public class DivisionController {
                 filter.setNameContains(nameContains);
                 filter.setWithCreatedBy(withCreatedBy);
                 filter.setWithUpdatedBy(withUpdatedBy);
+                filter.setSortField(sortField);
+                filter.setSortDirection(sortDirection);
                 Page<DivisionDto> divisions = divisionService.getAllDivisions(filter);
                 log.info("Ending DivisionController.list");
                 return ResponseDto.<List<DivisionDto>>builder().success(true)
